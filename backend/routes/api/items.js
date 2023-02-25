42cbf845f71f04042554d1c8e875d4e433d8c4eb
Tony Kharioki <antolisah@gmail.com>
2023-02-25 06:57:12 +0000

    generate image with openai backend function

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -5,6 +5,7 @@ var Comment = mongoose.model("Comment");
 var User = mongoose.model("User");
 var auth = require("../auth");
 const { sendEvent } = require("../../lib/event");
+const { generateImage } = require("../../lib/helper")
 
 // Preload item objects on routes with ':item'
 router.param("item", function(req, res, next, slug) {
@@ -139,13 +140,19 @@ router.get("/feed", auth.required, function(req, res, next) {
 
 router.post("/", auth.required, function(req, res, next) {
   User.findById(req.payload.id)
-    .then(function(user) {
+    .then(async function(user) {
       if (!user) {
         return res.sendStatus(401);
       }
 
-      var item = new Item(req.body.item);
+      // check if image was passed and if not generate one
+      if (!req.body.item.image) {
+        // generate image
+        req.body.item.image = await generateImage(req.body.item.title);
+      }
 
+      var item = new Item(req.body.item);
+      
       item.seller = user;
 
       return item.save().then(function() {
